3a6a9954e2e43a871a497ecec159da91663535ca
JigarSwam <jswaminarayan@gmail.com>
2025-02-20 20:56:31 -0800

    remove comments and console.log

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,11 +1,7 @@
 const palindromes = function (string) {
-    // filter out punctuation
-    // split based on " "
-    // make a copy and reverse the copy
-    // compare copy to original
+
     let punctuationless = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").toLowerCase();
     let spaceless = punctuationless.replace(/\s/g, "");
-    console.log("PUNC", spaceless);
 
     return spaceless.split("").reverse().join("") === spaceless;
 
