d755c6d5f747dec76e29d913bd993e99972571cc
JigarSwam <jswaminarayan@gmail.com>
2025-02-20 20:40:20 -0800

    complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,13 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    // filter out punctuation
+    // split based on " "
+    // make a copy and reverse the copy
+    // compare copy to original
+    let punctuationless = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").toLowerCase();
+    let spaceless = punctuationless.replace(/\s/g, "");
+    console.log("PUNC", spaceless);
+
+    return spaceless.split("").reverse().join("") === spaceless;
 
 };
 
