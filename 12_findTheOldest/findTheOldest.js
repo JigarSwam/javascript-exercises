3d7bd294bb797b8fd8788d5c3298f6b505de5da2
JigarSwam <jswaminarayan@gmail.com>
2025-02-20 21:47:38 -0800

    complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,13 @@
-const findTheOldest = function() {
+const getAge = (person) => {
+    person.yearOfDeath ??= new Date().getFullYear();
+
+    return person.yearOfDeath - person.yearOfBirth
+
+}
+
+const findTheOldest = function(people) {
+    people.sort((personA, personB) => getAge(personB) - getAge(personA));
+    return people[0];
 
 };
 
