cae0a189d06740558872b039e8de414c58be3c9c
JigarSwam <jswaminarayan@gmail.com>
2025-02-20 21:07:19 -0800

    complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(nth) {
+    let formattedNth
+
+    if (typeof nth !== "number") { formattedNth = parseInt(nth) } else formattedNth = nth;
+
+    if (nth === 0) { return 0 } 
+    if (nth < 0) { return "OOPS" }
+
+    let base = [0, 1];
+
+    // generate the fibonacci numbers up to the nth number
+    for(let i = 1; i < formattedNth; i++) {
+        base.push(base[i] + base[i-1])
+    }
+
+    return base[formattedNth];
 
 };
 
